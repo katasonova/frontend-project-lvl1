dfc12d110525a7f73a79bbca1e3dd98255d2ebd9
Elena Katasonova <elena.katasonova.2018@gmail.com>
2025-03-05 18:10:10 +0000

    Added readline-sync library

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!')
+
+// Wait for user's response.
+const name = readlineSync.question('May I have your name? ');
+console.log('Hi ' + name + '!');
